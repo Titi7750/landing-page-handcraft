865a6a3c79f375dffbafce2466d15dea4c554bb6
Titi <tristanfio05@gmail.com>
2024-12-20 11:39:45 +0100

    [DEBUG]: Setting config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import remix from 'vite-plugin-remix'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), remix()],
 })
