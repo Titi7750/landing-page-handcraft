59d74f88d5b098f1ff5ffd619883b84a17e2965c
Titi <tristanfio05@gmail.com>
2024-12-20 11:42:55 +0100

    [DEBUG]: Delete implementation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import remix from 'vite-plugin-remix'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), remix()],
+  plugins: [react()],
 })
