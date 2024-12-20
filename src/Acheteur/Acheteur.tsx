740dadbab6aba12ad99d993e18af2ac902349a35
Titi <tristanfio05@gmail.com>
2024-12-20 11:59:57 +0100

    [DEV]: Change image

--- src/Acheteur/Acheteur.tsx
+++ src/Acheteur/Acheteur.tsx
@@ -1,4 +1,14 @@
 function Acheteur() {
+  const image = '../public/img/Murano-Moyen-Smoky-Transparent-4-1200x1600.webp';
+  const image2 = '../public/img/laurettebroll_imageprincipale-7.jpg';
+  const image3 = '../public/img/02_Vase-plug-LEmbellie.jpeg';
+  const image4 = '../public/img/AdobeStock_484867951.jpeg';
+  const image5 = '../public/img/made-in-france.png';
+  const image6 = '../public/img/Frame_ecoresponsable.png';
+  const image7 = '../public/img/media-ceramiste-savoir 1.png';
+  const image8 = '../public/img/Image 2.png';
+  const image9 = '../public/img/1E9A1645-2-1-1152x1536 1.png';
+  const image10 = '../public/img/Assiette_plate_01_01-1.jpg';
 
   return (
     <>
@@ -14,37 +24,37 @@ function Acheteur() {
       <main>
         <div className='bg-main global-main-section'>
           <div className='global-first-section mb-5'>
-            <img className='w-25 h-50 rounded-5' src="../public/img/Murano-Moyen-Smoky-Transparent-4-1200x1600.webp" alt="Murano moyen" />
+            <img className='w-25 h-50 rounded-5' src={image} alt="Murano moyen" />
             <div>
               <h2 className='fs-30 fw-bold text-end'>Offrez-vous des<br />œuvres uniques et<br /> accessibles,<br /> <span className='color-kaki'>Made in France.</span></h2>
-              <img className='rounded-5' src="../public/img/laurettebroll_imageprincipale-7.jpg" alt="Fleche" />
+              <img className='rounded-5' src={image2} alt="Fleche" />
             </div>
           </div>
           <div className='global-second-section'>
             <div className=''>
               <h2 className='fs-30 fw-bold text-start'>Découvrez un <span className='color-kaki'>savoir-faire<br />artisanal</span>, dans le <span className='color-kaki'>respect<br />de l'environnement.</span></h2>
-              <img className='image-vase-fleur' src='../public/img/02_Vase-plug-LEmbellie.jpeg' alt='' />
+              <img className='image-vase-fleur' src={image3} alt='' />
             </div>
-            <img className='image-vase-main' src="../public/img/AdobeStock_484867951.jpeg" alt="Vase main" />
+            <img className='image-vase-main' src={image4} alt="Vase main" />
           </div>
         </div>
         <div className='global-made-in-france bg-kaki d-flex justify-content-around align-items-center text-center p-5'>
           <div>
-            <img className='image-made-france mb-4' src="../public/img/made-in-france.png" alt="Made in France" />
+            <img className='image-made-france mb-4' src={image5} alt="Made in France" />
             <h4 className='mb-4 color-white fs-20'>Certifié Made in France</h4>
             <p className='mb-4 color-white fs-16'>Nos artisans partenaires certifient l'origine tracée des produits.
               <br /> Sous cette marque collective, nos artisans bénéficient d'un rayonnement national.</p>
           </div>
           <div>
-            <img src='../public/img/Frame_ecoresponsable.png' alt="Eco-friendly" />
+            <img src={image6} alt="Eco-friendly" />
           </div>
         </div>
         <div className='global-gros-carre-blanc p-5'>
           <h3 className='fw-bold text-center mb-2'>Découvrez une communauté<br />de passionnés.</h3>
           <div className='d-flex justify-content-center align-items-center mb-100'>
-            <img src="../public/img/media-ceramiste-savoir 1.png" alt="" />
-            <img src="../public/img/Image 2.png" alt="" />
-            <img src="../public/img/1E9A1645-2-1-1152x1536 1.png" alt="" />
+            <img src={image7} alt="" />
+            <img src={image8} alt="" />
+            <img src={image9} alt="" />
           </div>
           <div className="global-carre-blanc bottom-5">
             <div className="col-md-6">
@@ -63,7 +73,7 @@ function Acheteur() {
         </div>
         <div className="bg-beige p-5">
           <div className="d-flex justify-content-center align-items-center">
-            <img src="../public/img/Assiette_plate_01_01-1.jpg" alt="Ceramic Plates" className="postion-relative rounded-5 w-25 h-25 me-5" />
+            <img src={image10} alt="Ceramic Plates" className="postion-relative rounded-5 w-25 h-25 me-5" />
             {/* <div className="contact-card z-3">
               <div className='bg-kaki p-5 color-white rounded-5'>
                 <h6 className='fw-bold mb-5'>Nous contacter</h6>
